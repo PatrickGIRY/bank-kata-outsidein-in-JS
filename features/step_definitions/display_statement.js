a69399a5e9f5d575eeb7a05c998f061f02bd588f
Patrick GIRY <patrickgiry@gmail.com>
2018-08-09 14:55:05 +0200

    * Implement then stepdef with page object

--- features/step_definitions/display_statement.js
+++ features/step_definitions/display_statement.js
@@ -1,13 +1,13 @@
 import { client } from 'nightwatch-cucumber'
-import { Given, When, Then} from 'cucumber'
+import { Given, When, Then, After} from 'cucumber'
 
 
-Given('a deposit of {int} on {int}-{int}-{int}', function (int, int2, int3, int4) {
+Given('a deposit of {int} on {int}-{int}-{int}', (amount, day, month, year) => {
   // Write code here that turns the phrase above into concrete actions
   return ;
 });
 
-Given('a withdrawal of {int} on {int}-{int}-{int}', function (int, int2, int3, int4) {
+Given('a withdrawal of {int} on {int}-{int}-{int}', (amount, day, month, year) => {
   // Write code here that turns the phrase above into concrete actions
   return ;
 });
@@ -19,5 +19,9 @@ When('he display the account statement', async () => {
 
 Then('the statement should have transactions in the following format:', async (dataTable) => {
   const accountStatement = client.page.accountStatement();
-  await accountStatement.waitForElementVisible('@accountStatement', 1000);
+  await accountStatement.shouldDisplayAccountStatement(dataTable);
+});
+
+After( async () => {
+  await client.end();
 });
--- page-objects/accountStatement.js
+++ page-objects/accountStatement.js
@@ -1,6 +1,25 @@
 
+const displayAccountStatementCommands = {
+  shouldDisplayAccountStatement(dataTable) {
+    const expectedValues = dataTable.raw();
+    return this.waitForElementPresent('@accountStatement')
+    .api.perform((div, done) => {
+        div.elements('css selector', 'tr', rows => {
+          rows.value.map((row, y) =>
+            div.elementIdElements(row.ELEMENT, 'css selector', 'th, td', cells => {
+              cells.value.map((cell, x) => div.elementIdText(cell.ELEMENT, text => {
+                div.assert.equal(text.value, expectedValues[y][x], JSON.stringify({row:x, cell:y}))
+              }));
+            }));
+        });
+      done();
+    });
+  }
+};
+
 module.exports = {
-  url: 'http://localhost:8080',
+  url: function() { return this.api.launchUrl; },
+  commands: [displayAccountStatementCommands],
   elements: {
     accountStatement: {
       selector: '#accountStatement',
